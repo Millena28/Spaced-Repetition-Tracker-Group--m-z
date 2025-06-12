2b5717deb3a271182dd22c3c48b4e5ce877a1bfc
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-06-12 12:50:04 +0100

    update test

--- common.test.mjs
+++ common.test.mjs
@@ -6,8 +6,8 @@ test("User count is correct", () => {
  const ids = getUserIDs();
     assert.equal(ids.length,5);
     ids.forEach(id => {
-      console.log(Array.isArray(id), ids);
-      assert.strictEqual(typeof id,"string", 'expected ID ${id} tobe string');
+      console.log(typeof (id), ids);
+      assert.strictEqual(typeof id,"string", `expected ID ${id} tobe string`);
     
   });
 });
